fd25d8eab8f3957e4836cf517cce3d5a58be5bd6
sainathbsnbsn <sainathbsn1729@gmail.com>
2025-02-10 19:24:54 +0530

    updated endpoint

--- Frontend/src/services/LoginService.js
+++ Frontend/src/services/LoginService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const EMPLOYEE_BASE_REST_API_URL = 'http://localhost:8888/expense';
+const EMPLOYEE_BASE_REST_API_URL = 'https://cbc8-2405-201-c007-41dd-3c11-fa3d-b1a5-7950.ngrok-free.app/expense';
+// const EMPLOYEE_BASE_REST_API_URL = 'http://localhost:8888/expense';
 
 
 export const authenticateUser = async (user) => {
